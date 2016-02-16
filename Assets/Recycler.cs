5c5b4ae37924ebcc631314a8801276a4e2ef7de1
rabidgremlin <jack@rabidgremlin.com>
2016-02-16 20:59:34 +1300

    Added parallax scrolling

---
+++ Assets/Recycler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recycler : MonoBehaviour {
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (transform.position.x < endPoint.position.x)
+        {
+            float gap = endPoint.position.x - transform.position.x;
+            transform.position = new Vector3(startPoint.position.x - gap, transform.position.y, transform.position.z);
+        }
+
+	}
+}
